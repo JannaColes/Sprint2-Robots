55d667d0fc77652291e0dbae21b64897165d77db
Malerie Earle <134969803+meekopants@users.noreply.github.com>
2023-08-07 22:04:09 -0230

    Updated - started fire fight function aug 7 2200h

--- HallwayCode.py
+++ HallwayCode.py
@@ -6,12 +6,15 @@ def ResetPoint():
     chassis_ctrl.move_with_distance(0, 0)
 
 
-
+def FireFunction():
+    
 # Starting Point (A)
 # Initialize all required settings such as speed, frequency, quantity and more
 # Function for starting program, settings, timer,
 def start():
     robot_ctrl.set_mode(rm_define.robot_mode_free)
+    # Will needs 2gb micro sd card to record
+    media_ctrl.record(1)
     gimbal_ctrl.set_rotate_speed(60)
     gimbal_ctrl.yaw_ctrl(250)
     gimbal_ctrl.pitch_ctrl(15)
@@ -30,24 +33,46 @@ def start():
 # Function to reset
 ResetPoint()
 # Function to move from end of B to the first room, C point
-chassis_ctrl.move_with_distance(0,5)
-chassis_ctrl.move_with_distance(0,1.5)
+chassis_ctrl.move_with_distance(0, 5)
+chassis_ctrl.move_with_distance(0, 1.5)
 # Total Move - 6.5 m
 
 
 # C Point
 # # Turn chassis 90 degrees
 chassis_ctrl.rotate_with_degree(rm_define.clockwise, 90)
-# # Enter room
-chassis_ctrl.move_with_distance(0,1.7)
+# # Enter room 1
+chassis_ctrl.move_with_distance(0, 2.08)
 
 
 # Function for room 1, if statement for room type,
 # # Function - type 1 - fire
+chassis_ctrl.rotate_with_degree(rm_define.clockwise, 90)
+chassis_ctrl.move_with_distance(0, 2.11)
+chassis_ctrl.rotate_with_degree(rm_define.clockwise, 90)
+vision_ctrl.enable_detection(rm_define.vision_detection_marker)
+vision_ctrl.set_marker_detection_distance(1)
+vision_ctrl.cond_wait(rm_define.cond_recognized_marker_letter_[F])
+
+# Flashing Blue Lights on Top
+led_ctrl.set_top_led(rm_define.armor_top_all, 0, 0, 255, rm_define.effect_flash)
+led_ctrl.set_flash(rm_define.armor_top_all, 2)
+# Flashing Red Lights on Top
+led_ctrl.set_top_led(rm_define.armor_top_all, 255, 0, 0, rm_define.effect_flash)
+led_ctrl.set_flash(rm_define.armor_top_all, 2)
+# Flashing Blue Lights on Bottom
+led_ctrl.set_bottom_led(rm_define.armor_bottom_all, 0, 0, 255, rm_define.effect_flash)
+led_ctrl.set_flash(rm_define.armor_bottom_all, 2)
+# Flashing Red Lights on Bottom
+led_ctrl.set_bottom_led(rm_define.armor_bottom_all, 255, 0, 0, rm_define.effect_flash)
+led_ctrl.set_flash(rm_define.armor_bottom_all, 2)
+
+gun_ctrl.set_fire_count(1, 1)
+gun_ctrl.fire_once()
 # # Function - type 2 - poison
 # # Function - type 3 - search and rescue person
 # # Function to leave room, return to C point
-chassis_ctrl.move_with_distance(0,1.7)
+chassis_ctrl.move_with_distance(0, 1.7)
 chassis_ctrl.rotate_with_degree(rm_define.clockwise, -90)
 # # Function to move from C point to D point
 
