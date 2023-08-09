2e245e8b109f965ddf0a599e43ce3330541413c1
JennyOKeyin <138630924+JennyOKeyin@users.noreply.github.com>
2023-08-09 17:49:40 -0230

    Update Marker1Function.py

--- Marker1Function.py
+++ Marker1Function.py
@@ -1,21 +1,24 @@
 # Program Comments
 # Written by:  Jennifer Oliver
-# Date Written: August 7, 2023 -
-# Program Description: Portion of code for Marker 1 function
+# Date Written: August 9, 2023 -
+# Program Description: Portion of code for Marker 1 function (Branch 1)
 
-# Function to move the gimbal and chassis 360 degrees in the opposite directions
-# as well as play a custom sound clip, at Position F
+
+# Function to move the gimbal and chassis 360 degrees in the opposite directions at Position F
 def Marker1():
     # chassis to rotate 360 counter clockwise (may need adjusting)
-    chassis_ctrl.rotate_with_degree(rm_define.clockwise, -360)
+    chassis_ctrl.rotate_with_degree(rm_define.anticlockwise, 360)
+    print("Chassis 360 rotation complete")
 
     # gimbal to rotate 360 clockwise
     gimbal_ctrl.yaw(360)
+    print("Gimbal 360 rotation complete")
 
     # add a sound-clip of robot saying "I'm number 1!" ????
-    media_ctrl.play_sound(rm_define.media_custom_audio_undefined)
+    # media_ctrl.play_sound(rm_define.media_custom_audio_undefined)
 
     # Recenter ???
     gimbal_ctrl.recenter()
+    print("Gimbal re-centered")
 
     # sleep to recenter chassis???
